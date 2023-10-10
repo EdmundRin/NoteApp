2145683d9cbaf3acd8e771db197cde1d0f24363f
cl101 <cl101@iu.edu>
2023-10-10 15:57:22 -0400

    more implement

--- app/src/main/java/com/example/note/EditNoteViewModel.kt
+++ app/src/main/java/com/example/note/EditNoteViewModel.kt
@@ -6,18 +6,25 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
-class EditNoteViewModel(val dao: NoteDao) : ViewModel() {
+class EditNoteViewModel(noteId: Long,val dao: NoteDao) : ViewModel() {
+    val note = dao.get(noteId)
     var newNoteName = ""
     var newNoteDescription = ""
     private val _navigateToList = MutableLiveData<Boolean>(false)
     val navigateToList: LiveData<Boolean>
         get() = _navigateToList
-    fun saveNote() {
+    fun addNote() {
         viewModelScope.launch {
             val note = Note()
             note.noteName = newNoteName
             note.noteDescription =newNoteDescription
             dao.insert(note)
+            _navigateToList.value = true
+        }
+    }
+    fun updateNote() {
+        viewModelScope.launch {
+            dao.update(note.value!!)
             _navigateToList.value = true
         }
     }
